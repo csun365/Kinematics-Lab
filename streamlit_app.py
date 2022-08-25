e5b60760b6e39cf8300108fb91244adc57101a18
csun365 <85044463+csun365@users.noreply.github.com>
2022-08-25 11:25:17 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import streamlit as st
 from scipy.optimize import curve_fit
 import numpy as np
-import matplotlib.pyplot as plt
 
 def func(x, a, b):
     return a * (1 - np.exp(-b * x))
