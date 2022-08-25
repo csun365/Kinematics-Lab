863fb19b559f575803c719d75a9f885528a07822
csun365 <85044463+csun365@users.noreply.github.com>
2022-08-25 11:20:06 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import scipy
+import pandas as pd
+import streamlit as st
 from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,5 +8,16 @@ import matplotlib.pyplot as plt
 def func(x, a, b):
     return a * (1 - np.exp(-b * x))
 
-popt, pcov = curve_fit(func, X, Y)
-print(popt)
+st.title("Kinematics Lab Exponential Curve Fitter")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+
+    X = df["Time (s)"]
+    Y = df["Speed (m/s)"]
+
+    popt, pcov = curve_fit(func, X, Y)
+    str = "v = %f(1 - e$^{%ft}$)" % (popt[0], popt[1])
+    st.write(str)
